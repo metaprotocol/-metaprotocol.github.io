f100b493906a793e16516e45da5ef65048191d20
Egor Rumyantsev <RumyantsevGT@ya.ru>
2021-11-18 04:12:55 +0300

    Update peerLink.js

--- ipfs/peerLink.js
+++ ipfs/peerLink.js
@@ -11415,16 +11415,7 @@ function emitTwo(handler, isFn, self, arg1, arg2) {
 
     .style('fill', COLORS.nodes.method)
      var peeer = 'https://gateway.ipfs.io/ipfs/'
-     function peer1 (
-              document.addEventListener('DOMContentLoaded', async () => {
-     const node = await Ipfs.create({ repo: 'ipfs-' + Math.random() })
-                      window.node = node
-     const peerInfos = await node.swarm.addrs(); 
-     let pir1 = peeer + peerInfos['0']['id']
-     return pir1
-                 });
-               }
-   peer1()
+
   g.append('a')
     .attr('class', 'link' )
     .attr('href', peeer)
